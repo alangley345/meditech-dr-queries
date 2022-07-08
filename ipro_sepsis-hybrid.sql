e57af3acca56665e5e26fdeef780c0ff7c9c92f8
Aaron Langley <alangley345@gmail.com>
2022-07-08 15:45:44 -0400

    Create ipro_sepsis-hybrid.sql

---
+++ ipro_sepsis-hybrid.sql
@@ -0,0 +1,65 @@
+DECLARE @facility VARCHAR(25);
+set @facility = 'facility_identifier'
+
+DECLARE @transfer_facility_id_receiving VARCHAR(25);
+set @transfer_facility_id_receiving = 'transfer_facility_id_receiving'
+
+DECLARE @transfer_facility_id_sending VARCHAR(25);
+set @transfer_facility_id_sending = 'transfer_facility_id_sending'
+
+
+SELECT ALL 
+       @facility																												 AS facility_identifier
+	  ,CONCAT(SUBSTRING([Name],0,5),SUBSTRING([Name],CHARINDEX(',',[Name],0)+1,2),RIGHT([UniquePublicIdentifier],4))			 AS unique_personal_identifier
+	  ,CONVERT(DATE,[ServiceDateTime])																							 AS arrival_dt
+      ,CONVERT(DATE,[BirthDateTime])																							 AS date_of_birth
+	  ,CONVERT(DATE,bar.[DischargeDateTime])																					 AS discharge_dt
+	  ,CASE	
+			WHEN [Response] = 'H'                  then 'E1'
+			WHEN [Response] = 'M'                  then 'E1.02'
+			WHEN [Response] = 'MA'                 then 'E1.02.001'
+			WHEN [Response] = 'CH'                 then 'E1.02.003'
+			WHEN [Response] = 'PR'				   then 'E1.06'
+			WHEN [Response] = 'C'                  then 'E1.07'																 
+			WHEN [Response] = 'NH'			       then 'E2'																	 																		 
+			ELSE 'E9'																											 
+	   END																														 AS ethnicity	
+	  ,[UnitNumber] as medical_record_number																					 
+	  ,[UnitNumber] as patient_control_number																					 
+	  ,CASE																														 
+			WHEN [RaceID] IN ('I','N')           then 'R1'																		 
+			WHEN [RaceID] IN ('A','C','J','K')   then 'R2' 																		 
+			WHEN [RaceID] = 'AI'                 then 'R2.01'																	 
+			WHEN [RaceID] = 'F'                  then 'R2.08'																	 
+			WHEN [RaceID] = 'B'                  then 'R3'																		 
+			WHEN [RaceID] = 'W'                  then 'R4.01.001'																 
+			WHEN [RaceID] = 'S'                  then 'R4.01.002'																 
+			WHEN [RaceID] IN ('CH','G')          then 'R4.02.001'																 
+			WHEN [RaceID] = 'OP'			     then 'R4.04'																	 
+			WHEN [RaceID] = 'W'                  then 'R5'																		 
+			ELSE 'R9'																											 
+	   END																														 AS race
+	  ,CASE																													 
+			WHEN  [DispositionID] = 'OHOSP'      then '1'
+			ELSE '0'
+	   END																														 AS transferred_out
+      ,CASE																													 
+			WHEN  [ArrivalID] = 'OH'      then '1'
+			ELSE '0'
+	   END																														 AS transferred_in	
+	  ,@transfer_facility_id_receiving																							 AS transfer_facility_id_receiving
+	  ,@transfer_facility_id_sending																							 As transfer_facility_id_sending
+
+  FROM [CH_MTLIVE].[dbo].[BarVisits]																							 AS bar	
+  
+  JOIN [CH_MTLIVE].[dbo].[AbsDrgDiagnoses] ON bar.VisitID = [AbsDrgDiagnoses].[VisitID]
+  JOIN [CH_MTLIVE].[dbo].[AdmVisitQueries] ON bar.VisitID = [AdmVisitQueries].[VisitID]
+  JOIN [CH_MTLIVE].[dbo].[AdmDischarge]    ON bar.VisitID = [AdmDischarge].[VisitID]
+  JOIN [CH_MTLIVE].[dbo].[AdmittingData]   ON bar.VisitID = [AdmittingData].[VisitID]
+
+  WHERE [ServiceDateTime]          IS NOT NULL
+  AND   [BirthDateTime]            IS NOT NULL
+  AND   bar.[DischargeDateTime]    IS NOT NULL
+  AND   QueryID = 'ETHNICITY'
+  AND   [UnitNumber] IS NOT NULL
+  AND   CONVERT(DATE,[ServiceDateTime])  >= '2022-01-01'
