674912e21de9736f02193a10fafd0687818604f9
Aaron Langley <alangley345@gmail.com>
2022-07-15 14:51:00 -0400

    Update ipro_sepsis-hybrid.sql

--- ipro_sepsis-hybrid.sql
+++ ipro_sepsis-hybrid.sql
@@ -58,8 +58,24 @@ SELECT ALL
 				ResultRW IN ('DETECTED','Detected','Positive','Positive') THEN ResultDateTime
 			ELSE NULL
 	   END																														 AS history_covid_dt					
-      ,[AbsDrgDiagnoses].[Diagnosis]																							 AS obesity			
-	  
+      ,CASE
+			WHEN 
+				[AbsDrgDiagnoses].[Diagnosis] IN (
+					'E66.1','E66.01','E66.09','E66.2','E66.8','E66.9','Z68.30','Z68.31','Z68.32','Z68.33',
+					'Z68.34','Z68.35','Z68.36','Z68.37','Z68.38','Z68.39','Z68.41','Z68.42','Z68.43',
+					'Z68.44','Z68.45','O99.214','O99.215','O99.210','O99.211','O99.212','O99.213'
+				) THEN '1'
+			ELSE '0'
+	   END																														 AS obesity			
+	  ,CASE
+			WHEN 
+				[AbsDrgDiagnoses].[Diagnosis] IN (
+					'O753','O0337','O0387','O0487','O0737','O0882','O85','O8604','O98811','O98812','O98813',
+					'O98819','O9882','O9883','O98911','O98912','O98913','O98919','O9892','O9893','O98511',
+					'O98512','O98513','O98519','O9852','O9853'
+				)THEN '1'
+			ELSE '0'
+	   END																														 AS pregnancy_status		
 
 -------------------------------------------------------------------------------------------------------------------------------------------------------
   FROM [CH_MTLIVE].[dbo].[BarVisits]																							 AS bar
@@ -74,18 +90,17 @@ SELECT ALL
 		,TestPrintNumberID   AS TestPrintNumberID
 		,ResultRW		     AS ResultRW
 		,ResultDateTime      AS ResultDateTime
-		 FROM [CH_MTLIVE].[dbo].[LabSpecimenTests] as CovidTests
+		 FROM [CH_MTLIVE].[dbo].[LabSpecimenTests] AS CovidTests
 		 WHERE TestPrintNumberID IN ('800.4051','100.0030','800.1036')
 		) AS CovidTests ON bar.[VisitID] = CovidTests.[VisitID]
   
  
-  WHERE   [ServiceDateTime]          IS NOT NULL
-  AND   [BirthDateTime]            IS NOT NULL
-  AND   bar.[DischargeDateTime]    IS NOT NULL
-  AND   QueryID = 'ETHNICITY'
-  AND   [UnitNumber]			   IS NOT NULL
-  AND   CONVERT(DATE,[ServiceDateTime])  >= '2022-01-01'
-  AND   [AbsDrgDiagnoses].Diagnosis like '%E66%'
+  WHERE   [ServiceDateTime]                  IS NOT NULL
+  AND     [BirthDateTime]                    IS NOT NULL
+  AND     bar.[DischargeDateTime]            IS NOT NULL
+  AND     QueryID = 'ETHNICITY'
+  AND     [UnitNumber]			             IS NOT NULL
+  AND     CONVERT(DATE,[ServiceDateTime])    >=          '2022-01-01'
 
   ORDER BY bar.[VisitID],(CASE
 			WHEN ResultRW IN ('DETECTED','Detected','Positive','Positive') THEN '1'
